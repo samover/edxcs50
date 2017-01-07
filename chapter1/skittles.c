c073b14839fc9d06f8c6193707e56b6fbbed33dd
Samuel Overloop <samuel.overloop@gmail.com>
2017-01-07 11:43:59 +0000

    Adds skittles

---
+++ chapter1/skittles.c
@@ -0,0 +1,46 @@
+/*
+ * skittles.c
+ * created by samover
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int MIN_SKITTLES = 0;
+int MAX_SKITTLES = 1023;
+
+int main(void)
+{
+    srand(time(NULL));
+    int skittles = rand() % 1024;
+    int answer = -1;
+    int numGuesses = 0;
+
+    printf("Guess the number I am thinking of (0 - 1023): ");
+
+    while (skittles != answer)
+    {
+        answer = get_int();
+        numGuesses += 1;
+
+        if (answer < MIN_SKITTLES) {
+            printf("Haha, you funny! Try again, and this time read the instructions: ");
+        }
+        else if (answer > MAX_SKITTLES) {
+            printf("Unfortunately my jar in not thát big. Try againg: ");
+        }
+        else if (answer < skittles)
+        {
+            printf("More! more! more!. Try again: ");
+        }
+        else if (answer > skittles)
+        {
+            printf("I wish! Try a lower number: ");
+        }
+    }
+
+    printf("Wow! Well done :-). It took you %d guesses.\n", numGuesses);
+    printf("See you soon!\n\n");
+}
